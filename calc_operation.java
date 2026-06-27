e69ff91d7efc691d2b4e9ad1c59b5e41111bef29
Soumya <aymousbeatz@gmail.com>
2026-06-27 12:42:33 +0530

    Add basic arithmetic operations in calc_operation.java
    Implement basic arithmetic operations: sum, difference, product, and division.

---
+++ calc_operation.java
@@ -0,0 +1,25 @@
+class Main {
+    public static void main(String[] args) {
+        int operand1 = 40;
+        int operand2 = 50;
+        
+        // SUM: 
+        System.out.println("SUM: " + (operand1 + operand2));
+        
+        // DIFFERENCE:
+        System.out.println("DIFFERENCE: " + (operand1 - operand2));
+        
+        // EXPAND:
+        System.out.println("EXPAND: " + (operand1 * operand2));
+        
+        // DIVISION:
+        System.out.println("DIVISION: " + (operand1 / operand2));
+        
+        
+        /*output:
+        SUM: 90
+        DIFFERENCE: -10
+        EXPAND: 2000
+        DIVISION: 0*/
+    }
+}
