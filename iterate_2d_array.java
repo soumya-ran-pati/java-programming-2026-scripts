fe9c4b7438220e232bf724442e649dab6c662ab1
Soumya <aymousbeatz@gmail.com>
2026-06-28 13:51:28 +0530

    Add Java program to iterate over a 2D array

---
+++ iterate_2d_array.java
@@ -0,0 +1,11 @@
+class Main {
+    public static void main(String[] args){
+        int[][] matrix = {{10, 20, 30}, {40, 50, 60}, {70, 80, 90}};
+        
+        for(int i=0; i<3; i++){
+            for(int j=0; j<3; j++){
+                System.out.println(matrix[i][j]);
+            }
+        }
+    }
+}
