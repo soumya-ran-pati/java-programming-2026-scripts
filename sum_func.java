8107a905cc2d8b871786c0fb9f2e45b11ab40f63
Soumya <aymousbeatz@gmail.com>
2026-06-28 13:32:44 +0530

    Add sum function to calculate the sum of two integers

---
+++ sum_func.java
@@ -0,0 +1,10 @@
+ class Main  {
+    public int sum(int a, int b){
+         return a + b;
+     } 
+     
+    public static void main(String[] args){
+        Main math = new Main();
+        System.out.println(math.sum(16, 42));
+     }
+ }
