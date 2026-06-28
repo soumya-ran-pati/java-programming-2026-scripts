2dbe17917d7d6e515855045a47f7fc516041c830
Soumya <aymousbeatz@gmail.com>
2026-06-28 12:19:04 +0530

    Add age-based eligibility check in Main class
    Implemented a basic eligibility check based on age input.

---
+++ check_eligibility.java
@@ -0,0 +1,16 @@
+class Main {
+    public static void main(String[] args){
+        
+        // initialize age:
+        int age = 19;
+        
+        // check eligibility:
+        if(age > 18){
+            System.out.println("You are eligible.");
+        }
+        else if (age < 18){
+            System.out.println("You are not eligible");
+          
+        }
+    }
+}
